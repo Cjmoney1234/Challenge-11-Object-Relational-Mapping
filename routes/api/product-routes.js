fbd06b011095ad094e40f79c353838227e99b792
Big_speed90 <zulma.jeff@yahoo.com>
2023-02-16 21:18:54 -0500

    added delete route to products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,6 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
+      attributes: ['id', 'product_name'],
       include: [{model: Tag}, {model: Category}],
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
 
@@ -117,8 +118,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!productData) {
+      res.status(404).json({ message:'No product with that id!' });
+      return;
+    } 
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
